4743e90a6cd814cc46a9ee630890442edb6e63c8
jawaher2 <79042182+jawaher2@users.noreply.github.com>
2021-02-27 06:26:10 +0300

    Add files via upload

---
+++ Answer3.java
@@ -0,0 +1,16 @@
+/**
+ * Created by shaker on 02/26/2021.
+ */
+public class Answer3 {
+    if(
+
+    Answer3()) {
+    throws new Empty();
+    }
+    int count = 1;
+    Node runner = head;
+while(runner.getNext()!=null) {
+        count++;
+        runner = runner.getNext();
+    }
+}
---
+++ Answer4.java
@@ -0,0 +1,24 @@
+/**
+ * Created by shaker on 02/26/2021.
+ */
+public class Answer4 {
+    if(
+
+    Answer4()) {
+    throws new Empty();
+    }
+    int count = 1;
+    Node hrunner = head;
+    Node trunner = tail;
+while(hrunner!=trunner && hrunner.getNext()!=trunner) {
+        count++; count++;
+        hrunner = hrunner.getNext();
+        trunner = trunner.getNext();
+    }
+if(hrunner!=trunner) {
+        // the pointers don't match - even number of elements
+        count--;
+    }
+
+// This handles the empty DLL, the one-element DLL, the two-element DLL, the three-element DLL
+}
